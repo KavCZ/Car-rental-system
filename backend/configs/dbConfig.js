2d68c8c2342e22b72ef3ccf7cad6a82946d98a7f
Martin Kavan <martin.kavan@atlas.cz>
2024-01-15 06:39:04 +0100

    snad funkcni db

--- backend/configs/dbConfig.js
+++ backend/configs/dbConfig.js
@@ -1,14 +1,21 @@
-import { Pool } from 'pg';
-import dotenv from 'dotenv';
+const { Pool } = require('pg')
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config({path:'../../.env'});
+};
 
-dotenv.config();
+let dbPassword = process.env.DB_PASSWORD
+
+dbPassword = String(dbPassword);
 
 const pool = new Pool({
   user: process.env.DB_USER,
   host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASSWORD,
+  name: process.env.DB_NAME,
+  password: dbPassword,
   port: process.env.DB_PORT,
+  minPoolSize: 5,
+  maxPoolSize: 10,
+  connectTimeout: 10000,
 });
 
-export default pool;
+module.exports = pool;
--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,45 @@
 // Import the http module to create an HTTP server and pool from dbConfig.js for database operations.
-import http from 'http';
-import pool from './configs/dbconfig.js';
+const http = require('http');
+const pool = require('./configs/dbconfig.js');
+const express = require("express");
+const { json } = require("express");
+const cors = require("cors");
+
+const app = express();
+
+app.use(json());
+app.use(cors());
 
 const port = 3000;
 
 // Create an HTTP server that responds to all incoming requests with the text "Server online".
 const server = http.createServer((_req, res) => {
-        res.statusCode = 200;
-        res.setHeader('Content-Type', 'text/plain');
-        res.end('Server online\n');
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Server online\n');
 });
+
+pool.connect((err) => {
+    if (err) {
+        console.log(err);
+    }
+    else {
+        console.log("Načítání databáze ... ");
+    }
+});
+/*
+const checkDatabaseConnection = () => {
+    pool.query('SELECT NOW()', (error, results) => {
+        if (error) {
+            console.error('Chyba při připojení k databázi:', error);
+        } else {
+            console.log('Úspěšně připojeno k databázi. Aktuální čas z databáze je:', results.rows[0].now);
+        }
+    });
+};
+
+// Volání funkce pro ověření připojení
+checkDatabaseConnection();
 
 // Execute an SQL query that retrieves all rows from the 'users' table.
 pool.query('SELECT * FROM users', (error, results) => {
@@ -34,8 +64,8 @@ pool.query('SELECT * FROM event', (error, results) => {
     }
     console.log(results.rows);
 });
-
+*/
 // Set the server to listen on the given port.
 server.listen(port, () => {
-        console.log(`Server running at http://localhost:${port}/`);
+    console.log(`Server running at http://localhost:${port}/`);
 });
